c78e85ba5ce63d161e3a516b84c2989923bfb589
Mihail Stefanov <mihail-stefanov@users.noreply.github.com>
2016-05-02 16:56:13 +0300

    Modified dashboard-controller.js to only load relevant project in the Affiliated projects section using $q.

--- app/scripts/controllers/dashboard-controller.js
+++ app/scripts/controllers/dashboard-controller.js
@@ -1,7 +1,7 @@
 angular.module('issueTrackingSystemApp')
-    .controller('DashboardController', ['$scope', 'authorisationService', 'projectService', 'issueService', function ($scope, authorisationService, projectService, issueService) {
+    .controller('DashboardController', ['$q', '$scope', 'authorisationService', 'projectService', 'issueService', function ($q, $scope, authorisationService, projectService, issueService) {
 
-        $scope.projectsCollection = projectService.getProjects({
+        $scope.fullProjectsCollection = projectService.getProjects({
             filter: '',
             pageSize: '1000',
             pageNumber: '1'
@@ -11,5 +11,28 @@ angular.module('issueTrackingSystemApp')
             orderBy: 'DueDate desc',
             pageSize: '1000',
             pageNumber: '1'
+        });
+
+        // Obtaining only the projects the user has an assigned issue to, or the ones the user is the leared of
+        $q.all([
+            $scope.fullProjectsCollection.$promise,
+            $scope.myIssuesCollection.$promise
+        ]).then(function () {
+            $scope.myProjectsCollection = {};
+            
+            // Adding projects the user is assigned to
+            $scope.myIssuesCollection.Issues.forEach(function(issue) {
+                $scope.myProjectsCollection[issue.Project.Id] = issue.Project.Name;
+            });
+            
+            // Adding projects the user is the lead of
+            
+            var currentUserName = authorisationService.getCurrentUser();
+            
+            $scope.fullProjectsCollection.Projects.forEach(function(project) {
+                if (project.Lead.Username == currentUserName.userName) {
+                    $scope.myProjectsCollection[project.Id] = project.Name;
+                }
+            });
         });
 }]);
