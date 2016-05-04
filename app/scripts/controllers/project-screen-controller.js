f6e1ba6c349207a259e3ed77feeeecaf52bf85d0
Mihail Stefanov <mihail-stefanov@users.noreply.github.com>
2016-05-04 19:14:35 +0300

    Modified the project-screen.html file to display the current project's information. Redesigned the the way the buttons on top to the dashboard and project pages are displayed.

--- app/scripts/controllers/project-screen-controller.js
+++ app/scripts/controllers/project-screen-controller.js
@@ -1,5 +1,10 @@
 angular.module('issueTrackingSystemApp')
-    .controller('ProjectScreenController', ['$scope', '$routeParams', '$http', 'authorisationService', 'projectService', function ($scope, $routeParams, $http, authorisationService, projectService) {
+    .controller('ProjectScreenController', ['$scope', '$routeParams', '$http', '$q', '$location', 'authorisationService', 'projectService', function ($scope, $routeParams, $http, $q, $location, authorisationService, projectService) {
+        
+        $scope.goToDashboard = function () {
+            $location.url('/');
+        };
+        
         $scope.projectId = $routeParams.projectId;
         
         $scope.obtainData = function () {
@@ -12,6 +17,26 @@ angular.module('issueTrackingSystemApp')
         }
         
         $scope.obtainData();
+        
+        $q.when($scope.currentProject.$promise).then(function() {
+                        
+            var convertToStringOfNames = function(arrayOfObjects) {
+                var names = '';
+                
+                arrayOfObjects.forEach(function(obj, index, array) {
+                    if (index < array.length - 1) {
+                        names += obj.Name + ', ';
+                    } else {
+                        names += obj.Name;
+                    }
+                });
+                
+                return names;
+            };
+            
+            $scope.currentProjectLabels = convertToStringOfNames($scope.currentProject.Labels);
+            $scope.currentProjectPriorities = convertToStringOfNames($scope.currentProject.Priorities);
+        });
         
         console.log($scope.currentProject);
         
